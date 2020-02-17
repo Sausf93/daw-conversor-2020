11dba0c50ba73f190f89b6bb0804eabe472fc86f
Saulo De La Santacruz <saulodlsf@gmail.com>
2020-02-17 18:44:24 +0000

    Prueba Travis

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Convertir Euros a dolares</h1>
+    <form action="POST" action="">
+    
+    <label for="euros">Euros: </label> <input type="number" name="eurdol">
+    <label for="result" name="result"></label>
+
+    </form>
+
+    <h1>Convertir Euros a yenes</h1>
+    <form action="POST" action="">
+    
+    <label for="euros">Euros: </label> <input type="number" name="euryen">
+    <label for="result" name="result"></label>
+
+    </form>
+</body>
+</html>
