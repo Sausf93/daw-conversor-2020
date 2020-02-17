9861498359b28bd6f9bd401993b34fe7cfb98875
Saulo De La Santacruz <saulodlsf@gmail.com>
2020-02-17 19:39:17 +0000

    V1.0.0

--- index.php
+++ index.php
@@ -6,19 +6,44 @@
 </head>
 <body>
 <h1>Convertir Euros a dolares</h1>
-    <form action="POST" action="">
+    <form method="POST" action="">
     
     <label for="euros">Euros: </label> <input type="number" name="eurdol">
     <label for="result" name="result"></label>
-
+    <input type="submit" value="Enviar">
     </form>
 
     <h1>Convertir Euros a yenes</h1>
-    <form action="POST" action="">
+    <form method="POST" action="">
     
     <label for="euros">Euros: </label> <input type="number" name="euryen">
     <label for="result" name="result"></label>
-
+    <input type="submit" value="Enviar">
     </form>
+
+    <?php
+    
+    require("src/Conversor.php");
+    
+    if(isset($_POST["eurdol"])){
+
+        $eur =intval($_POST["eurdol"]);
+        $Conversor =new Conversor();
+        $result= $Conversor->eurosADolares($eur);
+        echo"<h2>$result <h2";
+        $_POST["eurdol"]=null;
+    }
+
+    if(isset($_POST["euryen"])){
+
+        $eur =intval($_POST["euryen"]);
+        $Conversor =new Conversor();
+        $result= $Conversor->eurosADolares($eur);
+        echo"<h2>$result <h2";
+        $_POST["euryen"]=null;
+    }
+    
+    
+    ?>
 </body>
 </html>
